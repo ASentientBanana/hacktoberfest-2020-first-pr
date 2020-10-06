880e1e39ffae2f773e148a55182eb824c8c42bc3
Damir Zeric <dankmir@elfak.rs>
2020-10-06 21:07:09 +0200

    Added binary search with an example

---
+++ csharp/Binary search/binarySearch.cs
@@ -0,0 +1,43 @@
+using System; 
+public class Program
+{
+    static int binarySearch(int[] arr, int e, int min, int max) 
+    { 
+        if (max >= min)
+        {
+            int mid = (min + max) / 2;
+            
+            if (arr[mid] > e) 
+            {
+                return binarySearch(arr, e, min, mid - 1); 
+            }
+            else if (arr[mid] < e) 
+            {
+                return binarySearch(arr, e, mid + 1, max); 
+            }
+            else
+            {
+                return mid;
+            }
+        }        
+        return -1;
+    } 
+    
+    public static void Main()
+    {  
+        int[] arr = {1, 2, 5, 8, 9, 10, 25, 36};
+        
+        int e = 5;
+        int search = binarySearch(arr, e, 0, arr.Length-1);
+
+        if (search >= 0)
+        {
+            Console.WriteLine($"Element {e} is found on position {search}");  
+        }
+        else
+        {
+            Console.WriteLine($"Element {e} doesn't exist in array");  
+        }
+
+    }  
+}  
